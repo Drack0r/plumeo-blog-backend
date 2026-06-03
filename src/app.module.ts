6c6667fae26b7578c83c91a8ed2704d49ff8a5f4
Antonin <antochevolleau@gmail.com>
2026-06-03 19:18:49 +0200

    feat(auth): configure jwt authentication
    - Generate the auth module using the CLI
    - Install required authentication dependencies
    - Export UsersService from UsersModule
    - Import and configure UsersModule and JwtModule in AuthModule
    - Add JWT_SECRET to environment configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,9 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 import { ArticlesModule } from './articles/articles.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { ArticlesModule } from './articles/articles.module';
     MongooseModule.forRoot(process.env.MONGODB_URI!),
     UsersModule,
     ArticlesModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
